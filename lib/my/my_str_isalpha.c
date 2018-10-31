da14daed1270d05b8ff4ac6ed44b8724beb92f22
Thomas Boda <boda.thomas@orange.fr>
2018-10-31 11:40:06 +0100

    Add my_str_is_alpha function :abcd:

---
+++ lib/my/my_str_isalpha.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2018
+** my_str_isalpha
+** File description:
+** The task10 of the CPool Day06
+*/
+
+int my_str_isalpha(char const *str)
+{
+    int str_length = my_strlen(str);
+    int i = 0;
+
+    while (i != str_length) {
+        if (!((str[i] > 64 && str[i] < 91) ||
+        (str[i] > 96 && str[i] < 123)))
+            return (0);
+        i++;
+    }
+    return (1);
+}
