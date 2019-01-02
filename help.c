4e203de085fbff9308194a92222fd95905359688
Thomas Boda <thomas.boda@epitech.eu>
2019-01-02 13:46:01 +0100

    Add a main with the help function

---
+++ help.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** CLib
+** File description:
+** The help function of the lib
+*/
+
+#include <stdio.h>
+
+void help(void)
+{
+    printf("This program can not do anything for the moment.\n");
+}
---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2019
+** CLib
+** File description:
+** The main function of the example program of the CLib
+*/
+
+#include "lib.h"
+
+int main(int argc, char const *argv[])
+{
+    if (argc > 1) {
+        if (my_strcmp(argv[1], "-h") == 0) {
+            help();
+        }
+    }
+    return (0);
+}
