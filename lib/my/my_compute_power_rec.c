e5aebb698680bb937cac55a943318a336cac34ca
Thomas Boda <boda.thomas@orange.fr>
2019-02-25 13:08:48 +0100

    Optimize the my_compute_power_rec function :recycle:

--- lib/my/my_compute_power_rec.c
+++ lib/my/my_compute_power_rec.c
@@ -9,14 +9,9 @@
 
 int my_compute_power_rec(int nb, int p)
 {
-    int result = 1;
-    int i = 0;
-
-    if (p == 0) {
+    if (p == 0)
         return (1);
-    }
-    if (p < 0) {
+    if (p < 0)
         return (0);
-    }
     return (nb * my_compute_power_rec(nb, p - 1));
 }
