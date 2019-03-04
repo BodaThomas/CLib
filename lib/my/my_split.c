d7c773ba65a1051ff0a4bd6a4d6baa309a487d14
Thomas Boda <boda.thomas@orange.fr>
2019-03-04 17:27:55 +0100

    Rework the my_split function :scissors:

--- lib/my/my_split.c
+++ lib/my/my_split.c
@@ -6,52 +6,35 @@
 */
 
 #include <stdlib.h>
-#include "lib.h"
 
-char **my_split(char const *str, char deli)
-{
-    char **final = malloc(sizeof(char *) * (numb_of_words(str, deli) + 1));
-    int i = 0, word_numb = 0, i_numb = 0, size = 0;
-
-    final[numb_of_words(str, deli)] = NULL;
-    final[word_numb] = malloc(sizeof(char) * (word_size(str, i, deli) + 1));
-    while (i != my_strlen(str)) {
-        if (str[i] == deli) {
-            final[word_numb][i_numb] = '\0';
-            word_numb++;
-            i++;
-            size = word_size(str, i, deli);
-            final[word_numb] = malloc(sizeof(char) * (size + 1));
-            i_numb = 0;
-        }
-        final[word_numb][i_numb] = str[i];
-        i_numb++;
-        i++;
-    }
-    final[word_numb][i_numb] = '\0';
-    return (final);
-}
-
-int numb_of_words(char const *str, char delimiter)
+int count_delim(char const *str, char delim)
 {
     int i = 0;
-    int numb_of_words = 0;
+    int count = 0;
 
-    while (i != my_strlen(str)) {
-        if (str[i] == delimiter)
-            numb_of_words++;
+    while (str[i] != '\0') {
+        if (str[i] == delim)
+            count++;
         i++;
     }
-    return (numb_of_words);
+    return (count);
 }
 
-int word_size(char const *str, int pos, char delimiter)
+char **my_split(char *str, char const delim)
 {
-    int size = 0;
+    int i = 0;
+    int count = 0;
+    char **final = malloc(sizeof(char *) * count_delim(str, delim) + 1);
 
-    while (str[pos] != delimiter && str[pos] != '\0') {
-        pos++;
-        size++;
+    while (str[i] != '\0') {
+        if (i == 0 || str[i - 1] == '\0')
+            final[count] = str + i;
+        if (str[i] == delim) {
+            str[i] = '\0';
+            count++;
+        }
+        i++;
     }
-    return (size);
+    final[count + 1] = NULL;
+    return (final);
 }
