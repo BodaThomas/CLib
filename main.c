e554de86a218bceb4ac4bfae2b2cabf3b97824d4
Thomas Boda <boda.thomas@orange.fr>
2019-03-04 17:25:29 +0100

    Revert "Rework the my_split function :scissors:"
    This reverts commit 6be8bb356f30aa5958bfba24696d37250984554d.

--- main.c
+++ main.c
@@ -5,21 +5,11 @@
 ** The main function of the example program of the CLib
 */
 
-#include <string.h>
 #include "lib.h"
 #include "example.h"
 
 int main(int argc, char const *argv[])
 {
-    int i = 0;
-    char* str = strdup("bonjour_à_tous");
-    char**final = my_split(str, '_');
-    while (final[i] != '\0') {
-        my_putstr(final[i]);
-        my_putchar('\n');
-        i++;
-    }
-
     if (argc > 1) {
         if (my_strcmp(argv[1], "-h") == 0) {
             help();
